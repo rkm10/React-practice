bb2f0671a842587b8ec83f7c3101556fccc5f216
rkm10 <rajkumarmalluri08@gmail.com>
2024-05-11 14:05:11 +0530

    learnin jsx

--- src/components/Secondcomponent.jsx
+++ src/components/Secondcomponent.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 export default function Secondcomponent() {
-    return (
-        <div>Secondcomponent</div>
+    let raj = "it a new project"
+    let menu = (<ul>
+        <li className='test'>{raj}</li>
+        <li id='test'>please think</li>
+    </ul>)
+    function Menu() {
+        return <h1>hello world</h1>
+    }
+    return (menu, <Menu></Menu>
     )
 }
