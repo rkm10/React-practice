6e47f51b73db32b6677f8ef32ad1b7d92fdb5bc8
rkm10 <rajkumarmalluri08@gmail.com>
2024-05-11 16:02:29 +0530

    error

--- src/components/Firstcomponent.jsx
+++ src/components/Firstcomponent.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import Secondcomponent from './Secondcomponent'
 
+
 export default function Firstcomponent() {
-    return (
-        <div>
-            <h1>Firstcomponent</h1>
-            <h1>Firstcomponent</h1>
-            <Secondcomponent />
-        </div>
-    )
+
+
 }
--- src/components/Secondcomponent.jsx
+++ src/components/Secondcomponent.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react'
 
 export default function Secondcomponent() {
-    let raj = "it a new project";
+    let raj = "it a new project"
 
     function Menu() {
-        return (
-            <div>
-                <ul>
-                    <li className="test">{raj}</li>
-                    <li id="test">please think</li>
-                </ul>
-            </div>
-        );
+        return <div>
+
+            <ul>
+                <li className='test'>{raj}</li>
+                <li id='test'>please think</li>
+            </ul>
+        </div>
     }
-    return <Menu></Menu>;
+    return <Menu></Menu>
+
 }
