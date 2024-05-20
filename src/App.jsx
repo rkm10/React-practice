dc5744b91253b9a81fd8796c03bbfe2f9705ec06
rkm10 <rajkumarmalluri08@gmail.com>
2024-05-20 13:16:31 +0530

    new push

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import Display from "./components/Secondcomponent";
-import Form from "./forms";
+// import Display from "./components/Secondcomponent";
+import { lazy, Suspense, useState } from "react";
+
+const Form = lazy(() => import("./forms"))
 
 function App() {
 
+      const [show, setShow] = useState(false)
 
 
       return (
             <>
-                  <Form />
+                  <div className="text-center">
+                        <h1>Lazy loading</h1>
+                        <button className="btn btn-primary" onClick={() => {
+                              setShow(true)
+                        }}>open</button>
+                  </div>
+                  {
+                        show == true && (
+                              <Suspense>
+                                    <Form />
+                              </Suspense>
+                        )
+                  }
+
             </>
       );
 }
